0e741aaad72f3c43b58e4f90c8513cf1031cd3d0
Leandro Cunha <leandrocunha016@gmail.com>
2024-02-08 19:13:37 -0300

    Clean form after send name, email and password
    A toast show in screen after submit informations for register.

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Form, Input } from "@rocketseat/unform";
 import * as Yup from "yup";
@@ -12,26 +12,48 @@ const schema = Yup.object().shape({
         .email("Insira um e-mail válido")
         .required("O e-mail é obrigatório"),
     password: Yup.string()
-                .min(6, "No mímino 6 caracteres")
-                .required("A senha é obrigatória"),
+        .min(6, "No mímino 6 caracteres")
+        .required("A senha é obrigatória"),
 });
 
 export default function SignUp() {
     const dispatch = useDispatch();
     const loading = useSelector((state) => state.auth.loading);
+    const [formData, setFormData] = useState({});
+
     function handleSubmit({ name, email, password }) {
         dispatch(signUpRequest(name, email, password));
+        setFormData({}); // Limpar os campos do formulário após o envio
     }
+
+    function handleInputChange(event) {
+        const { name, value } = event.target;
+        setFormData({ ...formData, [name]: value });
+    }
+
     return (
         <>
-            <img src={logo} alt="GoBarber" title="Gobarber logo"/>
+            <img src={logo} alt="GoBarber" title="Gobarber logo" />
             <Form schema={schema} onSubmit={handleSubmit}>
-                <Input name="name" placeholder="Nome completo" />
-                <Input name="email" type="email" placeholder="Seu melhor e-mail" />
+                <Input
+                    name="name"
+                    placeholder="Nome completo"
+                    value={formData.name || ""}
+                    onChange={handleInputChange}
+                />
+                <Input
+                    name="email"
+                    type="email"
+                    placeholder="Seu melhor e-mail"
+                    value={formData.email || ""}
+                    onChange={handleInputChange}
+                />
                 <Input
                     name="password"
                     type="password"
                     placeholder="Sua senha secreta"
+                    value={formData.password || ""}
+                    onChange={handleInputChange}
                 />
                 <button type="submit">
                     {loading ? "Criando sua conta..." : "Criar"}
