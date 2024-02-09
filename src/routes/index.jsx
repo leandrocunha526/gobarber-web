4ad9c5c54c3e37db373ebb9596e761436f11ae42
Leandro Cunha <leandrocunha016@gmail.com>
2024-02-09 18:01:50 -0300

    Add Dashboard route in routes

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -3,12 +3,14 @@ import { Switch } from "react-router-dom";
 import Route from "./routes.jsx";
 import SignIn from "../pages/SignIn";
 import SignUp from "../pages/SignUp";
+import Dashboard from "../pages/Dashboard";
 
 export default function Routes() {
     return (
         <Switch>
             <Route path="/" exact component={SignIn} />
             <Route path="/register" component={SignUp} />
+            <Route path="/dashboard" component={Dashboard} isPrivate />
         </Switch>
     );
 }
